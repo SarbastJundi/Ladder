bc88984c34cd45bc929acb1b44b5d3a29e996dcc
Sarbast Jundi <sarbast.k.jundi@gmail.com>
2018-04-21 18:50:19 +0200

    first commit

---
+++ Ladder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/*
+ * Sarbast Jundi
+ * Högskolan i Skövde
+ * 
+ */
+
+namespace Ladder
+{
+    class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
+                long h = Int64.Parse(split[0]), v = Int64.Parse(split[1]);
+                Console.WriteLine(Math.Ceiling((double)h/Math.Sin((double)v/90 * (Math.PI)/2)));
+
+            }
+
+        }
+
+    }
+}
